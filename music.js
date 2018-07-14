37b64299b4c1ad84bfdcbe7fd5278f0aa3e07973
Nick Arnold <nicholas.d.arnold@gmail.com>
2018-07-14 12:52:43 -0700

    added mute btn

--- music.js
+++ music.js
@@ -1,9 +1,13 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
 var myBuffer;
+var g = context.createGain();
 
-play_clip = document.getElementById('play_clip');
+play_clip = document.getElementById('play_clip_btn');
 play_clip.addEventListener('click',clickHandler);
+
+mute_btn = document.getElementsByClassName('mute')[0];
+mute_btn.addEventListener('click',voiceMute);
 
 var request = new XMLHttpRequest();
 
@@ -19,19 +23,34 @@ request.onload = function() {
 }
 request.send();
 
+
 function onError() {
   console.log("error")
 }
 
 function playSound(buffer) {
-  var source = context.createBufferSource(), g = context.createGain();
+  var source = context.createBufferSource();
   source.buffer = buffer;
   source.start(0);
-  g.gain.value = 0.5;
+  g.gain.value = 1;
   source.connect(g);
   g.connect(context.destination);
 }
 
 function clickHandler(e) {
     playSound(myBuffer);
+}
+
+
+function voiceMute() {
+  console.log()
+  if(mute_btn.id == "") {
+    g.gain.setValueAtTime(0, context.currentTime);
+    mute_btn.id = "activated";
+    mute_btn.innerHTML = "Unmute";
+  } else {
+    g.gain.setValueAtTime(1, context.currentTime);
+    mute_btn.id = "";
+    mute_btn.innerHTML = "Mute";
+  }
 }
