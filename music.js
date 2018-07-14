0b81a9fc5a1070c18d0b03b1faff24abbe7d4980
Nick Arnold <nicholas.d.arnold@gmail.com>
2018-07-14 11:35:32 -0700

    Initial Commit

---
+++ music.js
@@ -0,0 +1,37 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+var myBuffer;
+
+clickme = document.getElementById('clickme');
+clickme.addEventListener('click',clickHandler);
+
+var request = new XMLHttpRequest();
+
+request.open('GET', '/audiofiles/piano1.wav', true);
+
+request.responseType = 'arraybuffer';
+
+// Decode asynchronously
+request.onload = function() {
+  context.decodeAudioData(request.response, function(theBuffer) {
+    myBuffer = theBuffer;
+  }, onError);
+}
+request.send();
+
+function onError() {
+  console.log("error")
+}
+
+function playSound(buffer) {
+  var source = context.createBufferSource(), g = context.createGain();
+  source.buffer = buffer;
+  source.start(0);
+  g.gain.value = 0.5;
+  source.connect(g);
+  g.connect(context.destination);
+}
+
+function clickHandler(e) {
+    playSound(myBuffer);
+}
